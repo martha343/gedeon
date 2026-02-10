44e22e00086c5d778b340911f5b6a4b99f9a98c4
martha343 <marthamulungi6@gmail.com>
2026-02-10 11:32:15 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+require("dotenv").config();
+
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+app.use(express.json());
+
+app.use("/api", authRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port " + process.env.PORT);
+});
